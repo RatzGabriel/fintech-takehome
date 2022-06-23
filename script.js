f3d1d22e76f2a7fc488b0eeac738c8c7cd774602
Edgar Carrillo <ecarrillo046@gmail.com>
2022-06-23 14:15:57 -0700

    feat: add error handling

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-console.log('Hello World!');
+(function() {
+  const form = document.getElementById('loginForm');
+  form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    $.ajax({
+      method: 'POST',
+      url: './helpers/proxy.php',
+      data: $(this).serialize(),
+    })
+    .then((response) => {
+      const data = JSON.parse(response);
+      console.log(data);
+      const emailInputElem = document.getElementById('emailInputContainer');
+      const emailErrorElem = emailInputElem.getElementsByClassName('errorMsg')[0];
+      emailErrorElem.textContent = '';
+      emailErrorElem.style.display = 'none';
+
+      const passwordInputElem = document.getElementById('passwordInputContainer');
+      const passwordErrorElem = passwordInputElem.getElementsByClassName('errorMsg')[0];
+      passwordErrorElem.textContent = '';
+      passwordErrorElem.style.display = 'none';
+
+      if (data.jsonCode === 200) {
+        /* success */
+      } else if (data.jsonCode === 404) {
+        emailErrorElem.textContent += 'This email doesn\'t match any account.';
+        emailErrorElem.style.display = 'block';
+      } else if (data.jsonCode === 401) {
+        passwordErrorElem.textContent += 'Incorrect password. Please try again.';
+        passwordErrorElem.style.display = 'block';
+      }
+    })
+    .fail((err) => {
+      console.log('API Error: ', err);
+    });
+  })
+})();
